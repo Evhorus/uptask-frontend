2406eb5912e1774eafd69f81d312cfe788a2f3ae
Sebastian Velasco <sesbas778@gmail.com>
2024-06-14 15:30:14 -0500

    Arreglos de las notificaiones y redirigir al login al  confirmar la cuenta

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,27 +3,26 @@ import { useAuth } from "@/hooks/useAuth";
 import { Navigate, Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 export default function AuthLayout() {
-  const { data, isError, isLoading } = useAuth();
+  const { data, isLoading } = useAuth();
   if (isLoading) return "Cargando...";
   if (data) {
     return <Navigate to="/" />;
   }
-  if (isError)
-    return (
-      <>
-        <div className="bg-gray-800 min-h-screen">
-          <div className="py-5 mx-auto sm:w-[500px]">
-            <Logo />
-            <div className="m-5 sm:m-10">
-              <Outlet />
-            </div>
+  return (
+    <>
+      <div className="bg-gray-800 min-h-screen">
+        <div className="py-5 mx-auto sm:w-[500px]">
+          <Logo />
+          <div className="m-5 sm:m-10">
+            <Outlet />
           </div>
         </div>
-        <ToastContainer
-          pauseOnHover={false}
-          pauseOnFocusLoss={false}
-          autoClose={8000}
-        />
-      </>
-    );
+      </div>
+      <ToastContainer
+        pauseOnHover={false}
+        pauseOnFocusLoss={false}
+        autoClose={8000}
+      />
+    </>
+  );
 }
