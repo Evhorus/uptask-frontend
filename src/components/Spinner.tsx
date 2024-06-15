4745d50cbc71a582233edd5d7373c9d55d572512
Sebastian Velasco <sesbas778@gmail.com>
2024-06-15 07:32:38 -0500

    Se desactiva el drag N drop y se agrega Spinner para las pantallas de carga

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ import { DotLoader } from "react-spinners";
 export default function Spinner() {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <DotLoader color="#836fe3" />;
+      <DotLoader color="#836fe3" />
     </div>
   );
 }
